8622f120a670b52248beccfcb8cdeb277d4ab6ca
aragantinaveen <naveenaraganti9010@gmail.com>
2025-02-12 14:11:40 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -4,14 +4,15 @@
 
 
 #! /bin/bash
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
-chmod +x kops-linux-amd64 kubectl
-mv kubectl /usr/local/bin/kubectl
-mv kops-linux-amd64 /usr/local/bin/kops
-
-aws s3api create-bucket --bucket naveen22.flm.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket naveen22.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://naveen22.flm.k8s.local
-kops create cluster --name naveen.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /var/local/bin/
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+mv kops /usr/local/bin/
+aws s3 mb s3://naveen11.flm
+export KOPS_STATE_STORE=s3://naveen11.flm
+kops create cluster --name naveen.k8s.local --zones us-east-1a,us-east-1b --master-size t2.medium --master-count 1 --master-volume-size 30 --node-size t2.micro --node-count 2 --node-volume-size 20
 kops update cluster --name naveen.k8s.local --yes --admin
