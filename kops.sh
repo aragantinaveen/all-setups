02ef95c4ed8740ca2bbf3184bd103d815f9461db
aragantinaveen <naveenaraganti9010@gmail.com>
2025-02-12 12:47:49 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -13,5 +13,5 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket naveen11.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket naveen111.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://naveen111.k8s.local
-kops create cluster --name naveen.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+kops create cluster --name naveen.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
 kops update cluster --name naveen.k8s.local --yes --admin
