6cf95feab751cd1f84175ba00e1a69ed3c7e45e3
aragantinaveen <naveenaraganti9010@gmail.com>
2025-02-12 13:29:31 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -4,14 +4,15 @@
 
 
 #! /bin/bash
+aws configure
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket naveen11.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket naveen11.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://naveen111.k8s.local
-kops create cluster --name naveen.k8s.local --zones us-east-1a,us-east-1b --master-size t2.medium --master-count 1 --master-volume-size 30 --node-size t2.micro --node-count 2 --node-volume-size 20
+aws s3api create-bucket --bucket naveen22.flm.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket naveen22.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://naveen22.flm.k8s.local
+kops create cluster --name naveen.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
 kops update cluster --name naveen.k8s.local --yes --admin
