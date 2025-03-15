234a28f5037a5c318372bba8181dadcfa09dbecc
aragantinaveen <naveenaraganti9010@gmail.com>
2025-03-15 12:29:02 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -13,7 +13,7 @@ mv kubectl /usr/local/bin/
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops
 mv kops /usr/local/bin/
-aws s3 mb s3://naveen22.flm
-export KOPS_STATE_STORE=s3://naveen22.flm
-kops create cluster --name naveen2.k8s.local --zones us-east-1a,us-east-1b --master-size t2.medium --master-count 1 --master-volume-size 30 --node-size t2.micro --node-count 2 --node-volume-size 20
-kops update cluster --name naveen2.k8s.local --yes --admin
+aws s3 mb s3://naveen.ang
+export KOPS_STATE_STORE=s3://naveen.ang
+kops create cluster --name naveen3.k8s.local --zones us-east-1a,us-east-1b --master-size t2.medium --master-count 1 --master-volume-size 30 --node-size t2.micro --node-count 2 --node-volume-size 20
+kops update cluster --name naveen3.k8s.local --yes --admin
